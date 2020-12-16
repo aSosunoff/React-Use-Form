721e151fb5122aec8d4c7fd83ecc12484f650998
asosunoff <al.sosunoff@gmail.com>
2020-12-16 21:06:54 +0300

    ашч

--- demo/components/UI/button/baseButton/BaseButton.js
+++ demo/components/UI/button/baseButton/BaseButton.js
@@ -4,61 +4,61 @@ import cn from "classnames";
 import styles from "./BaseButton.module.scss";
 
 const BaseButton = ({
-	tag,
-	type,
-	disabled,
-	invalid,
-	invalidMessage,
-	classNameContainer,
-	classNameButton,
-	children,
-	...props
+  tag,
+  type,
+  disabled,
+  invalid,
+  invalidMessage,
+  classNameContainer,
+  classNameButton,
+  children,
+  ...props
 }) => {
-	const classDisabled = { [styles.disabled]: disabled };
+  const classDisabled = { [styles.disabled]: disabled };
 
-	if (disabled) {
-		props.onClick = (e) => e.preventDefault();
-	}
+  if (disabled) {
+    props.onClick = (e) => e.preventDefault();
+  }
 
-	const buttonElement = React.createElement(
-		tag,
-		{
-			key: "button",
-			className: cn(classNameButton, styles.button, classDisabled),
-			type,
-			...props,
-		},
-		children
-	);
+  const buttonElement = React.createElement(
+    tag,
+    {
+      key: "button",
+      className: cn(classNameButton, styles.button, classDisabled),
+      type,
+      ...props,
+    },
+    children
+  );
 
-	const invalidElement = invalid
-		? React.createElement(
-				"small",
-				{
-					key: "error",
-					className: cn(styles.button__invalid, classDisabled),
-				},
-				invalidMessage
-		  )
-		: null;
+  const invalidElement = invalid
+    ? React.createElement(
+        "small",
+        {
+          key: "error",
+          className: cn(styles.button__invalid, classDisabled),
+        },
+        invalidMessage
+      )
+    : null;
 
-	return React.createElement("div", { className: classNameContainer }, [
-		buttonElement,
-		invalidElement,
-	]);
+  return React.createElement("div", { className: classNameContainer }, [
+    buttonElement,
+    invalidElement,
+  ]);
 };
 
 BaseButton.defaultProps = {
-	tag: "button",
-	type: "button",
+  tag: "button",
+  type: "button",
 };
 
 BaseButton.propTypes = {
-	tag: PropTypes.oneOf(["button", "a", "Link"]),
-	type: PropTypes.oneOf(["button", "submit", "reset"]),
-	disabled: PropTypes.bool,
-	invalid: PropTypes.bool,
-	invalidMessage: PropTypes.string,
+  tag: PropTypes.oneOf(["button", "a", "Link"]),
+  type: PropTypes.oneOf(["button", "submit", "reset"]),
+  disabled: PropTypes.bool,
+  invalid: PropTypes.bool,
+  invalidMessage: PropTypes.string,
 };
 
 export default BaseButton;
--- demo/components/layout/emptyLayout/EmptyLayout.js
+++ demo/components/layout/emptyLayout/EmptyLayout.js
@@ -1,20 +1,18 @@
 import React from "react";
-import styles from "./EmptyLayout.module.scss";
 import cn from "classnames";
+import styles from "./EmptyLayout.module.scss";
 
-const EmptyLayout = ({ children }) => {
-	return (
-		<div
-			className={cn(
-				styles["grid-layout"],
-				styles["grid-layout_center"],
-				"blue-grey",
-				"darken-3"
-			)}
-		>
-			{children}
-		</div>
-	);
-};
+const EmptyLayout = ({ children }) => (
+  <div
+    className={cn(
+      styles["grid-layout"],
+      styles["grid-layout_center"],
+      "blue-grey",
+      "darken-3"
+    )}
+  >
+    {children}
+  </div>
+);
 
 export default EmptyLayout;
