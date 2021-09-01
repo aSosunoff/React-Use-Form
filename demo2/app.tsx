e4b199db41bd9ef4724090ae78bac06f97055bee
asosunoff <al.sosunoff@gmail.com>
2021-09-01 17:46:22 +0300

    fix

--- demo2/app.tsx
+++ demo2/app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useForm, InitialFormType } from "../src";
 import styles from "./app.module.scss";
 
@@ -13,6 +13,12 @@ const init_form: InitialFormType<"name" | "age"> = {
 
 const EMPTY_OBJ = {};
 
+const init_form_for_create: InitialFormType<"nameFieldForCreate"> = {
+  nameFieldForCreate: {
+    value: "",
+  },
+};
+
 export const App = () => {
   const form = useForm(init_form);
 
@@ -24,49 +30,62 @@ export const App = () => {
 
   /* form.setValues<keyof typeof init_form>({}); */
 
+  /*  */
+
+  const formCreate = useForm(init_form_for_create);
+
+  const createNewField = useCallback(
+    (fieldName: string) => {
+      form.addFields({
+        [fieldName]: {
+          value: "",
+          validation: (value: string) => {
+            if (value.trim().length === 0) {
+              return {
+                errorMessage: "ошибка. поле обязательно для заполнения",
+              };
+            }
+          },
+        },
+      });
+    },
+    [form]
+  );
+
   return (
     <div className={styles.container}>
-      <button
-        onClick={() => {
-          form.addFields({
-            newField: {
-              value: "",
-              validation: (value: string) => {
-                if (value.trim().length === 0) {
-                  return {
-                    errorMessage: "ошибка. поле обязательно для заполнения",
-                  };
-                }
-              },
-            },
-          });
-        }}
-      >
-        Создать новое поле
-      </button>
+      <div className={styles["field-create"]}>
+        <input
+          value={formCreate.handlers.nameFieldForCreate.value}
+          onKeyUp={(ev) => {
+            if (ev.key === "Enter") {
+              createNewField(formCreate.values.nameFieldForCreate);
+            }
+          }}
+          onChange={({ target }) => {
+            formCreate.handlers.nameFieldForCreate.onChange(target.value);
+          }}
+        />
 
-      <button
-        onClick={() => {
-          form.reset();
-        }}
-      >
-        Сбросить
-      </button>
+        <button
+          onClick={() => {
+            createNewField(formCreate.values.nameFieldForCreate);
+          }}
+        >
+          Создать новое поле
+        </button>
+      </div>
 
-      <button
-        onClick={() => {
-          form.clear();
-        }}
-      >
-        Очистить
-      </button>
+      <button onClick={form.reset}>Сбросить</button>
+
+      <button onClick={form.clear}>Очистить</button>
 
       {Object.entries(handlers).map(([name, config], index) => (
         <div key={index} className={styles.field}>
           <div>{name}</div>
 
           <input
-            name={name}
+            tabIndex={index}
             value={config.value}
             onChange={({ target }) => {
               config.onChange(target.value);
@@ -86,6 +105,8 @@ export const App = () => {
           )}
         </div>
       ))}
+
+      {form.isInvalidForm ? <div>На форме есть ошибки</div> : null}
     </div>
   );
 };
